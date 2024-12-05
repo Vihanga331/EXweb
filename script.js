7d0251a8bc59ffba2360562f0257d42bef34d66a
Vihanga331 <148764168+Vihanga331@users.noreply.github.com>
2024-12-05 14:45:43 +0530

    Add files
    Add files for develop web page

---
+++ script.js
@@ -0,0 +1,11 @@
+const home = document.querySelector('#home');
+const about = document.querySelector('#about');
+const skills = document.querySelector('#skills');
+
+
+
+
+
+
+
+home.addEventListener()
